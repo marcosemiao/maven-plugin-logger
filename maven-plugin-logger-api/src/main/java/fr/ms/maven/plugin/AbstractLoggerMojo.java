84ee0e0766dc0e75b34628ef6db7765a3ce8194c
Marco Semiao <marco.semiao@gmail.com>
2015-08-03 17:04:38 +0200

    correct

--- maven-plugin-logger-api/src/main/java/fr/ms/maven/plugin/AbstractLoggerMojo.java
+++ maven-plugin-logger-api/src/main/java/fr/ms/maven/plugin/AbstractLoggerMojo.java
@@ -1,11 +1,36 @@
+/*
+ * Copyright 2015 Marco Semiao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package fr.ms.maven.plugin;
 
 import java.util.Iterator;
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.logging.Log;
 
+/**
+ *
+ * @see <a href="http://marcosemiao4j.wordpress.com">Marco4J</a>
+ *
+ *
+ * @author Marco Semiao
+ *
+ */
 public abstract class AbstractLoggerMojo extends AbstractMojo {
 
     @Override
@@ -27,11 +52,19 @@ public abstract class AbstractLoggerMojo extends AbstractMojo {
 	private final static MojoLogger mojoLogger = createInstance();
 
 	private final static MojoLogger createInstance() {
-	    final ServiceLoader<MojoLogger> load = ServiceLoader.load(MojoLogger.class);
+	    try {
+		final ServiceLoader<MojoLogger> load = ServiceLoader.load(MojoLogger.class);
 
-	    final Iterator<MojoLogger> iterator = load.iterator();
-	    final MojoLogger singleton = iterator.next();
-	    return singleton;
+		final Iterator<MojoLogger> iterator = load.iterator();
+		final MojoLogger singleton = iterator.next();
+
+		if (iterator.hasNext()) {
+		    throw new ServiceConfigurationError("Plusieurs implémentations de MojoLogger trouvées");
+		}
+		return singleton;
+	    } catch (final Exception e) {
+		throw new ServiceConfigurationError("Aucune implémentation de MojoLogger trouvée", e);
+	    }
 	}
     }
 }
--- maven-plugin-logger-slf4j/src/main/java/fr/ms/maven/slf4j/impl/Slf4jMojoLogger.java
+++ maven-plugin-logger-slf4j/src/main/java/fr/ms/maven/slf4j/impl/Slf4jMojoLogger.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Marco Semiao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package fr.ms.maven.slf4j.impl;
 
 import org.apache.maven.plugin.logging.Log;
@@ -6,6 +22,14 @@ import org.slf4j.impl.StaticLoggerBinder;
 import fr.ms.maven.plugin.MojoLogger;
 import fr.ms.util.ServiceProvider;
 
+/**
+ *
+ * @see <a href="http://marcosemiao4j.wordpress.com">Marco4J</a>
+ *
+ *
+ * @author Marco Semiao
+ *
+ */
 @ServiceProvider
 public class Slf4jMojoLogger implements MojoLogger {
 
